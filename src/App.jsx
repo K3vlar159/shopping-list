91d28db2a3a87c212034806467914e1fa4e036d0
K3vlar159 <david.jergus159@gmail.com>
2025-11-10 19:43:11 +0100

    first shopping list prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[tasks, setTasks] = useState([])
+  const[inputValue, setInputValue] = useState('')
+
+  const handleAddTask = (e) => {
+    e.preventDefault()
+    if(inputValue.trim() === '') return
+
+    const newTask = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false
+    }
+
+    setTasks([...tasks, newTask])
+    setInputValue('')
+  }
+
+const handleToggleTask = (id) => {
+  setTasks(tasks.map(task => task.id === id 
+      ? { ...task, completed: !task.completed }
+      : task
+  ))
+}
+
+const handleDeleteTask = (e,id) => {
+  e.stopPropagation()
+  setTasks(tasks.filter(task => task.id !== id))
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      <h1>📋 Task Manager</h1>
+      <form onSubmit={handleAddTask}>
+        <input 
+          type="text"
+          value= {inputValue}
+          onChange = {(e) => setInputValue(e.target.value)}
+          placeholder = "New task.." 
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <p>Number of tasks: {tasks.length}</p>
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <li
+            key ={task.id}
+            onClick={() => handleToggleTask(task.id)}
+          >
+            <span onClick={() => handleToggleTask(task.id)}>
+              {task.completed ? '✅' : '⬜'} {task.text}
+            </span>
+            <button onClick={(e) => handleDeleteTask(e,task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
